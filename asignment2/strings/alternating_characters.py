f98a227c6266b0aec058c91aac463f65c7c5e06a
nocid <64286255+nocid@users.noreply.github.com>
2022-12-11 18:31:13 +0100

    last commit

--- asignment2/strings/alternating_characters.py
+++ asignment2/strings/alternating_characters.py
@@ -1,6 +1,6 @@
 #https://www.hackerrank.com/challenges/alternating-characters/problem
 
-"""
+
 import math
 import os
 import random
@@ -34,43 +34,5 @@ if __name__ == '__main__':
 
         fptr.write(str(result) + '\n')
 
-    fptr.close()"""
-            
-n = int(input())
-permu = ''.join(input().split())
-permu = list(map(int, permu))
-print(permu)
-minx = min(permu)
-maxx = max(permu)
-incr = [minx]
-decr = []
-for x in range(n):
-    if permu[x] == minx:
-        posmin = x
-    if permu[x] == maxx:
-        posmax = x
-print(posmax)
-#increasing subsequence
-for x in range(posmin, n-1):
-   # if pos == n or pos == n-1:
-        #if permu[pos] > permu[]
-    if x == posmin:
-        pos = posmin
-    if x != posmin:
-        mini = min(permu[(pos+1): n])
-        for i in range(n):
-            if permu[i] == mini:
-                pos = i 
-        incr.append(permu[pos])
-#print(incr)
+    fptr.close()
 
-for x in range(n-1,posmax , -1):
-    if x == posmax:
-        pos = posmax
-    if x != posmax:
-        maxi = max(permu[(pos+1): n])
-        for i in range(n):
-            if permu[i] == maxi:
-                pos = i 
-        decr.append(permu[pos])
-print(*decr)
